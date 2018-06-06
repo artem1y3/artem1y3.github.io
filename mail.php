3ceeeec7a40a61a4919a26ba88eb34fcfda59399
artem1y3 <37960104+artem1y3@users.noreply.github.com>
2018-06-07 00:46:07 +0300

    Next

--- mail.php
+++ mail.php
@@ -1,4 +1,8 @@
-<?php
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
 
 $fio = $_POST['fio'];
 $fio = htmlspecialchars($fio);
@@ -35,22 +39,38 @@ $pc = htmlspecialchars($pc);
 $pc = urldecode($pc);
 $pc = trim($pc);
 
-		
-			$text .= "Ваше ФИО: $fio\n\n";
-			$text .= "Телефон: $tel\n";
-			$text .= "Площадь комнаты, м2: $area\n";
-			$text .= "Ориентация окон: $window\n";
-			$info = "From: test@mail.ru\r\n";
-			// if(mail("228zmei@gmail.com", "Обратная связь", $text , $info)){
-			//    echo 'Сообщение отправлено';
-			// } else {
-			// 	echo 'сообщение не отправлено';
-			// }
-			if (mail("228zmei@gmail.com", "Заявка с сайта", "ФИО:".$fio,"From: 228zmei@mail.ru \r\n"))
-			 {     echo "сообщение успешно отправлено"; 
-			} else { 
-			    echo "при отправке сообщения возникли ошибки";
-			}?>
-				
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'petrovskov.shura@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '1dsgs23g'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('petrovskov.shura@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('artembk739@gmail.com');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка';
+$text .= "Ваше ФИО: $fio\n\n";
+$text .= "Телефон: $tel\n";
+$text .= "Площадь комнаты, м2: $area\n";
+$text .= "Ориентация окон: $window\n";
+$info = "From: petrovskov.shura@mail.ru\r\n";
+$mail->Body    = $text;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    // header('location: thank-you.html');
+}
 ?>
