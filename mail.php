573716398e48ac4878973d7f825935eca4694fdc
artem1y3 <37960104+artem1y3@users.noreply.github.com>
2018-06-06 22:40:57 +0300

    Next commit

---
+++ mail.php
@@ -0,0 +1,48 @@
+<?php
+
+$fio = $_POST['fio'];
+$fio = htmlspecialchars($fio);
+$fio = urldecode($fio);
+$fio = trim($fio);
+
+$tel = $_POST['tel'];
+$tel = htmlspecialchars($tel);
+$tel = urldecode($tel);
+$tel = trim($tel);
+
+$tel = $_POST['tel'];
+$tel = htmlspecialchars($tel);
+$tel = urldecode($tel);
+$tel = trim($tel);
+
+$area = $_POST['area'];
+$area = htmlspecialchars($area);
+$area = urldecode($area);
+$area = trim($area);
+
+$window = $_POST['window'];
+$window = htmlspecialchars($window);
+$window = urldecode($window);
+$window = trim($window);
+
+$tv = $_POST['tv'];
+$tv = htmlspecialchars($tv);
+$tv = urldecode($tv);
+$tv = trim($tv);
+
+$pc = $_POST['pc'];
+$pc = htmlspecialchars($pc);
+$pc = urldecode($pc);
+$pc = trim($pc);
+
+		
+			$text .= "Ваше ФИО: $fio\n\n";
+			$text .= "Телефон: $tel\n";
+			$text .= "Площадь комнаты, м2: $area\n";
+			$text .= "Ориентация окон: $window\n";
+			$info = "From:Сайт\r\n";
+			if(mail("228zmei@gmail.com", "Обратная связь", $text , $info)){
+			   header('Location:http://www.forum-georus.org/?in=1'); 
+			}
+			
+				?>
