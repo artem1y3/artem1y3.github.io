4281cc63e106436b56463c5463b6f603252e6b3b
artem1y3 <37960104+artem1y3@users.noreply.github.com>
2018-06-11 18:05:38 +0300

    Next commit

--- js/js.js
+++ js/js.js
@@ -1,3 +1,5 @@
+var modal = document.getElementsByClassName('modal-win')[0];
+
 $('.slider').slick({
 	dots: true,
 	arrows: false
@@ -6,7 +8,16 @@ $('.slider').slick({
 $('.close-win').click(function(){
 	$('.modal-win').css('display','none');
 })
+// $('.modal-win').click(function(){
+// 	$('.modal-win').css('display','none');
+// })
 
 $('.open-win').click(function () {
 	$('.modal-win').css('display','block');
 })
+
+window.onclick = function (event) {
+	if(event.target == modal){
+		modal.style.display = 'none';
+	}
+}
