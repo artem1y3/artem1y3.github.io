c1adf46385ae82630ec78fc762cf317e9793f7ba
artem1y3 <37960104+artem1y3@users.noreply.github.com>
2018-06-11 15:48:29 +0300

    Next commit

--- js/js.js
+++ js/js.js
@@ -2,3 +2,11 @@ $('.slider').slick({
 	dots: true,
 	arrows: false
 });
+
+$('.close-win').click(function(){
+	$('.modal-win').css('display','none');
+})
+
+$('.open-win').click(function () {
+	$('.modal-win').css('display','block');
+})
